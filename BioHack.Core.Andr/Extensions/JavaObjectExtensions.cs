9422221301de83e498293bb984c682152bcac69a
sydneyos <sydreg@weenet.com>
2013-07-05 11:07:10 -0700

    Ignore files

---
+++ BioHack.Core.Andr/Extensions/JavaObjectExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace BioHack.Core.Andr.Extensions
+{
+    public static class JavaObjectExtensions
+    {
+        public static T Cast<T>(this Java.Lang.Object obj) where T : class
+        {
+            var propertyInfo = obj.GetType().GetProperty("Instance");
+            return propertyInfo == null ? null : propertyInfo.GetValue(obj, null) as T;
+        }
+    }
+}
